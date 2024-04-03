625a45e06f096c14fd53d2063e1f7df20cb886c9
taniacruzz <tanicruz112@gmail.com>
2024-04-02 21:55:32 -0300

    feat: spring setup

---
+++ springAPI4/springAPI4/src/main/java/com/fatec/springAPI4/SpringApi4Application.java
@@ -0,0 +1,13 @@
+package com.fatec.springAPI4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApi4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApi4Application.class, args);
+	}
+
+}
