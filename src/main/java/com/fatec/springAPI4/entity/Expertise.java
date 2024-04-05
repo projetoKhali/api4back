68d6206f9385ba9ea61ec2436b6fdcb92bed320c
taniacruzz <tanicruz112@gmail.com>
2024-04-05 11:08:03 -0300

    fix: retirada de importação de 'not null'

--- src/main/java/com/fatec/springAPI4/entity/Expertise.java
+++ src/main/java/com/fatec/springAPI4/entity/Expertise.java
@@ -10,8 +10,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-import javax.validation.constraints.NotNull;
-
 @Getter @Setter @AllArgsConstructor
 @Entity
 @Table
