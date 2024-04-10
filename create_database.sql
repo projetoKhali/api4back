48e2c0d115b8d4c6d4f3af0f45ca93064315dba1
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-04-10 17:30:22 -0300

    chore: remove commented code in `create_database` sql script

--- create_database.sql
+++ create_database.sql
@@ -1,21 +1,4 @@
 -- Table: Partner
-/*CREATE TABLE Partner (
-    pt_id varchar PRIMARY KEY,
-    pt_company_id varchar NOT NULL UNIQUE,
-    pt_name varchar NOT NULL,
-    pt_admin_name varchar NOT NULL,
-    pt_admin_email varchar NOT NULL,
-    pt_compliance boolean NOT NULL,
-    pt_credit boolean NOT NULL,
-    pt_status boolean NOT NULL,
-    pt_membership date DEFAULT CURRENT_DATE NOT NULL,
-    pt_slogan varchar NOT NULL,
-    pt_country varchar NOT NULL,
-    pt_city varchar NOT NULL,
-    pt_number varchar NOT NULL,
-    pt_address varchar NOT NULL
-)*/
--- Cria a tabela Partner
 CREATE TABLE Partner (
     pt_id BIGINT PRIMARY KEY,
     pt_name VARCHAR(255),
