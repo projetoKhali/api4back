b3752c42767bb2645aa2f7524e28f9fe04973aa6
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-04-30 20:33:37 -0300

    Remove unused imports in ExpertiseController.java and PartnerController.java

--- src/main/java/com/fatec/springapi4/controller/ExpertiseController.java
+++ src/main/java/com/fatec/springapi4/controller/ExpertiseController.java
@@ -1,6 +1,5 @@
 package com.fatec.springapi4.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/fatec/springapi4/controller/PartnerController.java
+++ src/main/java/com/fatec/springapi4/controller/PartnerController.java
@@ -1,7 +1,6 @@
 package com.fatec.springapi4.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
