060fa258265d22b40fcfaaea3a965eaa2a22c19e
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-05-04 20:01:03 -0300

    chore: Add TrackMetricsController, TrackMetrics entity, TrackMetricsRepository, ITrackMetricsService, and TrackMetricsService

---
+++ src/main/java/com/fatec/springapi4/entity/TrackMetrics.java
@@ -0,0 +1,37 @@
+package com.fatec.springapi4.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "track_metrics")
+public class TrackMetrics {
+    @Id
+    @Column (name = "tk_id")
+    private Long tkId;
+
+    @Column (name = "expertise_count")
+    private Long expertiseCount;
+
+    @Column (name = "qualifier_count")
+    private Long qualifierCount;
+
+    @Column (name = "partner_count")
+    private Long partnerCount;
+    
+    @Column (name = "avg_expertise_completion_time")
+    private Double ExpertiseCompletionTime;
+    
+    @Column (name = "avg_qualifier_completion_time")
+    private Double QualifierCompletionTime;
+    
+    @Column (name = "avg_qualifier_completion_percentage")
+    private Integer QualifierCompletionPercentage;
+}
---
+++ src/main/java/com/fatec/springapi4/repository/TrackMetricsRepository.java
@@ -0,0 +1,18 @@
+package com.fatec.springapi4.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.fatec.springapi4.entity.TrackMetrics;
+
+@Repository
+public interface TrackMetricsRepository extends JpaRepository<TrackMetrics, Long>{
+    
+    @Query("select t from TrackMetrics t where t.tkId = :id")
+    List<TrackMetrics> findByTrackId(@Param("id") Long tkId);
+
+}
