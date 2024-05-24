b3399f854a4b8c4ace3e3c80efff1b36ec7a55ce
Ryan Lanziloti <pitagoraslanz@gmail.com>
2024-05-23 22:33:56 -0300

    fix view

---
+++ src/main/java/com/fatec/springapi4/service/IPartnerMetricsService.java
@@ -0,0 +1,12 @@
+package com.fatec.springapi4.service;
+
+import java.util.List;
+
+import com.fatec.springapi4.entity.PartnerMetrics;
+
+public interface IPartnerMetricsService {
+    
+    public List<PartnerMetrics> listPartnerMetrics();
+    public List<PartnerMetrics> listPartnerMetricById(Long id);
+
+}
---
+++ src/main/java/com/fatec/springapi4/service/PartnerMetricsService.java
@@ -0,0 +1,24 @@
+package com.fatec.springapi4.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fatec.springapi4.entity.PartnerMetrics;
+import com.fatec.springapi4.repository.PartnerMetricsRepository;
+
+@Service
+public class PartnerMetricsService implements IPartnerMetricsService {
+
+    @Autowired
+    private PartnerMetricsRepository partnerMetricsRepository;
+
+    public List<PartnerMetrics> listPartnerMetrics(){
+        return partnerMetricsRepository.findAll();
+    }
+    
+    public List<PartnerMetrics> listPartnerMetricById(Long id){
+        return partnerMetricsRepository.findByPartnerId(id);
+    }
+}
