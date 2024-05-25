7ffd16b15d0480ddc144da01fd09c6a8dd328d4c
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-05-25 00:02:13 -0300

    fix: use `POST` method for `allPartnerMetrics` endpoint in `PartnerMetricsController`

--- src/main/java/com/fatec/springapi4/controller/PartnerMetricsController.java
+++ src/main/java/com/fatec/springapi4/controller/PartnerMetricsController.java
@@ -1,18 +1,18 @@
 package com.fatec.springapi4.controller;
 
 import com.fatec.springapi4.entity.PartnerMetrics;
+import com.fatec.springapi4.schema.PartnerMetricsRequest;
 import com.fatec.springapi4.service.IPartnerMetricsService;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,19 +25,21 @@ public class PartnerMetricsController {
     @Autowired
     private IPartnerMetricsService iPartnerMetricsService;
 
-    @GetMapping
+    @PostMapping
     public Page<PartnerMetrics> allPartnerMetrics(
-            @RequestParam(value = "partialName", required = false) String partialName,
-            @PageableDefault(sort = { "name" }, value = 10) Pageable pageable) {
+            @RequestBody PartnerMetricsRequest body,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(value = "sort", defaultValue = "name") String sortField) {
 
-        Sort sort = pageable.getSort();
+        Sort sort = Sort.by(sortField);
 
         if (sort == null || sort.isEmpty() || sort.toString().contains("string")) {
             sort = Sort.by("name");
         }
 
         return iPartnerMetricsService.listPartnerMetrics(
-                partialName, PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), sort));
+                body.getPartialName(), PageRequest.of(page > 0 ? page : 0, size > 0 ? size : 10, sort));
     }
 
     @GetMapping(value = "/{id}")
