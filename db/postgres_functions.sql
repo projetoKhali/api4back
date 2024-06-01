4b687dfc51fb1d4a9eef78685acd8f21e8b370f9
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-06-01 12:50:51 -0300

    feat: Add functions for tracking completed qualifiers and expertise counts

--- db/postgres_functions.sql
+++ db/postgres_functions.sql
@@ -16,9 +16,8 @@ RETURN COUNT;
 END;
 $$ LANGUAGE plpgsql;
 
--- TODO
--- deveria retornar quantidade de qualificadores conclídos ou não (retornando número errado)
-CREATE OR REPLACE FUNCTION qualifier_completed_count(tk INTEGER, is_completed BOOLEAN)
+-- contador de qualificadores completos na track
+CREATE OR REPLACE FUNCTION qualifier_completed_count(tk INTEGER)
 RETURNS INTEGER AS $$
 DECLARE
     COUNT INTEGER;
@@ -27,13 +26,54 @@ BEGIN
     INTO COUNT
     FROM Partner_Qualifier AS pt_ql
     JOIN Expertise_Qualifier AS ex_ql ON pt_ql.ql_id = ex_ql.ql_id
-    WHERE ex_ql.ex_id IN 
-        (is_completed AND pt_ql.pt_ql_complete_date IS NOT NULL) OR
-        (NOT is_completed AND pt_ql.pt_ql_complete_date IS NULL) AND
-        ex_ql.ex_id IN
-            (SELECT ex_id 
-            FROM Expertise AS ex
-            WHERE ex.tk_id = tk);
-    RETURN count;
+    where ex_ql.ex_id IN 
+		(SELECT ex_id 
+        FROM Expertise AS ex
+        WHERE ex.tk_id = tk);
+RETURN COUNT as ql_completed;
+END;
+$$ LANGUAGE plpgsql;
+
+-- contador de expertises na track
+CREATE OR REPLACE FUNCTION expertise_count(tk INTEGER)
+RETURNS INTEGER AS $$
+DECLARE
+    COUNT INTEGER;
+BEGIN
+    SELECT COUNT(ex.tk_id)
+    INTO COUNT
+    FROM Expertise AS ex 
+    WHERE ex.tk_id = tk;
+RETURN COUNT;
+END;
+$$ LANGUAGE plpgsql;
+
+-- contador de expertises completas na track
+CREATE OR REPLACE FUNCTION expertise_completed_count(tk INTEGER)
+RETURNS INTEGER AS $$
+DECLARE
+    COUNT INTEGER;
+BEGIN
+    SELECT COUNT (pt_ex.pt_ex_complete_date)
+    INTO COUNT
+    FROM Partner_Expertise AS pt_ex
+    JOIN Expertise AS ex ON pt_ex.ex_id = ex.ex_id
+    WHERE ex.tk_id = tk;
+RETURN COUNT as ex_completed;
+END;
+$$ LANGUAGE plpgsql;
+
+-- média de tempo de conclusão dos qualificadores
+CREATE OR REPLACE FUNCTION avg_expertise_completion_time(tk INTEGER)
+RETURNS INTEGER AS $$
+DECLARE
+    AVG_TIME INTEGER;
+BEGIN
+    SELECT AVG((pt_ex.pt_ex_complete_date - pt_ex.pt_ex_insert_date))
+    INTO AVG_TIME
+    FROM Partner_Expertise AS pt_ex 
+    JOIN Expertise AS ex ON pt_ex.ex_id = ex.ex_id 
+    WHERE ex.tk_id = tk;
+RETURN AVG_TIME;
 END;
 $$ LANGUAGE plpgsql;
