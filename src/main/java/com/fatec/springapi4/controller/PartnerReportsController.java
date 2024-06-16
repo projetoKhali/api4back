0cb024ab57eb273cbaa1a87bc1236867c8d216de
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-06-16 17:24:43 -0300

    style: sort imports of `PartnerReportsRepository`

--- src/main/java/com/fatec/springapi4/controller/PartnerReportsController.java
+++ src/main/java/com/fatec/springapi4/controller/PartnerReportsController.java
@@ -1,20 +1,19 @@
 package com.fatec.springapi4.controller;
 
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.fatec.springapi4.entity.PartnerReports;
 import com.fatec.springapi4.repository.PartnerReportsRepository;
 import com.fatec.springapi4.service.IPartnerReportsService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
